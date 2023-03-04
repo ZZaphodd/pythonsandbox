7d7f225d626ab267b7dd99368475b21a91329edd
ZZaphodd <mephistopeles84@gmail.com>
2023-03-04 10:56:12 +0900

    remove comments

--- ncav_datagen.py
+++ ncav_datagen.py
@@ -26,16 +26,9 @@ if __name__ == '__main__':
 
     if not os.path.exists(collectedFilePath):
         krxStocks = kkmbaekkrx.getKrxStocks()
-        # print(krxStocks)
-        # print(kkmbaekkrx.parseFnguideFinance(kkmbaekkrx.getFnguideFinance('005930')))
         collected = pd.DataFrame()
-
-        # for _, stock_code in krxStocks.iterrows():
-            # result = code_to_dict(stock_code.code)
-            # collected = pd.concat([ collected, pd.DataFrame.from_records( [ result ])])
         
         with mp.Pool(processes = mp.cpu_count()) as pool:
-            # dictList = pool.map(code_to_dict, list(krxStocks['code']))
             dictList = pool.map(code_to_dict, list(krxStocks['code']))
 
         collected = pd.DataFrame.from_records(dictList)
